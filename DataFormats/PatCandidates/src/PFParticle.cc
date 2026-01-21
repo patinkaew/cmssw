b51ab1017c607291bf5992d180c7de8cc265d9da
Patin Inkaew <earthpatin@gmail.com>
2026-01-21 20:15:54 +0100

    add constructor from reco::PFCandidate

--- DataFormats/PatCandidates/src/PFParticle.cc
+++ DataFormats/PatCandidates/src/PFParticle.cc
@@ -5,6 +5,10 @@
 
 using namespace pat;
 
+/// constructor from reco::PFCandidate
+PFParticle::PFParticle(const reco::PFCandidate& aPFParticle)
+    : PATObject<reco::PFCandidate>(aPFParticle) {}
+
 /// constructor from PFParticleType
 PFParticle::PFParticle(const edm::RefToBase<reco::PFCandidate>& aPFParticle)
     : PATObject<reco::PFCandidate>(aPFParticle) {}
